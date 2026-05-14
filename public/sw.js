d70c432b8362d2f4df403cda6892fd65bdcb73cb
Ichi <issei.yasuda1@gmail.com>
2026-05-14 21:00:53 +0900

    Release Ver. 2.5

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE_NAME = 'rits-oic-map-static-v12';
-const RUNTIME_CACHE_NAME = 'rits-oic-map-runtime-v12';
+const STATIC_CACHE_NAME = 'rits-oic-map-static-v13';
+const RUNTIME_CACHE_NAME = 'rits-oic-map-runtime-v13';
 const APP_SHELL_URLS = [
   '/',
   '/index.html',
